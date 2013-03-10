f44792168c0e4c2d8b55a2112f6db14a4ad424f3
grandbora <bora.tunca@yahoo.com>
2013-03-10 18:30:15 +0100

    refactoring

--- src/Module/tax_calculator.rb
+++ src/Module/tax_calculator.rb
@@ -1,2 +1,20 @@
 module TaxCalculator
+
+  def calculate_tax(shelf_price, tax_percent)
+    real_tax = calculate_real_tax(shelf_price, tax_percent)
+    return 0 if real_tax == 0
+
+    round_up(real_tax)
+  end
+
+  private
+  def calculate_real_tax(shelf_price, tax_percent)
+    shelf_price * tax_percent / 100
+  end
+
+  def round_up(real_tax)
+    (real_tax * 20).ceil().to_f / 20
+  end
+
+
 end 
--- src/product.rb
+++ src/product.rb
@@ -18,24 +18,14 @@ class Product
   end
 
   def sales_tax
-    real_tax = calculate_tax(@sales_tax_percent)
-    return 0 if real_tax == 0
-
-    (real_tax * 20).ceil().to_f / 20
+    calculate_tax(@shelf_price, @sales_tax_percent)
   end
 
   def import_tax
-    real_tax = calculate_tax(@import_tax_percent)
-    return 0 if real_tax == 0
-
-    (real_tax * 20).ceil().to_f / 20
+    calculate_tax(@shelf_price, @import_tax_percent)
   end
 
   private
-  def calculate_tax(tax_percent)
-    @shelf_price * tax_percent / 100
-  end
-
   def set_sales_tax_percent(sales_tax_percent)
     @sales_tax_percent = sales_tax_percent
   end
